c6c3c8c8a5b94daca5aaea8dc2a9c14be8cb95a6
simonsen <simonsen@localhost.localdomain>
2018-08-10 22:34:43 +0200

    General overhaul

--- ColourString.py
+++ ColourString.py
@@ -1,6 +1,4 @@
-def colour(inputString='', colour='Default', bold=False, underline=False, invert=False):
-    if inputString == '':
-        print('''\033[1mSyntax:
+helpString = '''\033[1mSyntax:
 \033[0mcs.colour(inputString, colour=Default, bold=False, underline=False, invert=False)
 
 \033[1mCurrently supported colours:\033[0m
@@ -26,7 +24,31 @@ cs.colour("Just to underline my point", underline=True)
 
 cs.colour("Let\'s go all in!", "bright-yellow", bold=True, underline=True, invert=True)
 --> \033[93;1;4;7mLet\'s go all in!\033[0m
-\033[0m''')
+\033[0m'''
+
+colourCodes = {
+    'RED':'\033[31',
+    'YELLOW':'\033[33',
+    'GREEN':'\033[32',
+    'BLUE':'\033[34',
+    'BLACK':'\033[30',
+    'WHITE':'\033[37',
+    'MAGENTA':'\033[35',
+    'CYAN':'\033[36',
+    'BRIGHT-RED':'\033[91',
+    'BRIGHT-YELLOW':'\033[93',
+    'BRIGHT-GREEN':'\033[92',
+    'BRIGHT-BLUE':'\033[94',
+    'BRIGHT-BLACK':'\033[90',
+    'BRIGHT-WHITE':'\033[97',
+    'BRIGHT-MAGENTA':'\033[95',
+    'BRIGHT-CYAN':'\033[96'
+    # 'DEFAULT':'\033['
+}
+
+def colour(inputString='', colour='Default', bold=False, underline=False, invert=False):
+    if inputString == '':
+        print(helpString)
         return
     if not type(colour) == str:
         print('''\033[33mError: Wrongly typed cs.colour()
@@ -49,44 +71,6 @@ Invert is of %s but should be of <type \'bool\'>
 To get help and a full list of colours, call cs.colour() with no arguments\033[0m''' % (str(type(invert))))
         return inputString
     capCol = colour.upper()
-    if capCol == 'RED':
-        colourCode = '\033[31'
-    elif capCol == 'YELLOW':
-        colourCode = '\033[33'
-    elif capCol == 'GREEN':
-        colourCode = '\033[32'
-    elif capCol == 'BLUE':
-        colourCode = '\033[34'
-    elif capCol == 'BLACK':
-        colourCode = '\033[30'
-    elif capCol == 'WHITE':
-        colourCode = '\033[37'
-    elif capCol == 'MAGENTA':
-        colourCode = '\033[35'
-    elif capCol == 'CYAN':
-        colourCode = '\033[36'
-    elif capCol == 'BRIGHT-RED':
-        colourCode = '\033[91'
-    elif capCol == 'BRIGHT-YELLOW':
-        colourCode = '\033[93'
-    elif capCol == 'BRIGHT-GREEN':
-        colourCode = '\033[92'
-    elif capCol == 'BRIGHT-BLUE':
-        colourCode = '\033[94'
-    elif capCol == 'BRIGHT-BLACK':
-        colourCode = '\033[90'
-    elif capCol == 'BRIGHT-WHITE':
-        colourCode = '\033[97'
-    elif capCol == 'BRIGHT-MAGENTA':
-        colourCode = '\033[95'
-    elif capCol == 'BRIGHT-CYAN':
-        colourCode = '\033[96'
-    elif capCol == 'DEFAULT':
-        colourCode = '\033['
-    else:
-        print('''\033[33mError: wrongly formatted cs.colour()
-(colour %s not recognized)
-To get help and a full list of colours, call cs.colour() with no arguments\033[0m''' % (colour))
-        return inputString
+    colourCode = colourCodes.get(capCol, '\033[')
     prefix = colourCode + ';1'*bold + ';4'*underline + ';7'*invert + 'm'
     return prefix + inputString + '\033[0m'
